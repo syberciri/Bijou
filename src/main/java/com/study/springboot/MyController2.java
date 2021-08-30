8e2986bae240426ad0b53e5d22eaf0b9327138a7
sy <syyyp0@gmail.com>
2021-08-30 09:52:51 +0900

    210830 성연

--- src/main/java/com/study/springboot/MyController2.java
+++ src/main/java/com/study/springboot/MyController2.java
@@ -1,5 +1,22 @@
 package com.study.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
 public class MyController2 {
 
+	@RequestMapping("/")
+	public String root( RedirectAttributes redirect ) {
+		
+		return "redirect:member_list";
+	}
+	
+	@RequestMapping("memberList")
+	public String memberList( Model model ) {
+		
+		return "memberList";
+	}
 }
